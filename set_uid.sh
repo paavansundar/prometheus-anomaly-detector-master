9df0662392f29f06139b0ecad8f4486f4d0fada4
Duvvuri <DUVVUPA1@novartis.net>
2023-02-09 12:05:01 +0530

    Initial commit

---
+++ set_uid.sh
@@ -0,0 +1,6 @@
+if [ `id -u` -ge 10000 ]; then
+    cat /etc/passwd | sed -e "s/^$NB_USER:/builder:/" > /tmp/passwd
+    echo "$NB_USER:x:`id -u`:`id -g`:,,,:/home/$NB_USER:/bin/bash" > /tmp/passwd
+    cat /tmp/passwd > /etc/passwd
+    rm /tmp/passwd
+fi
